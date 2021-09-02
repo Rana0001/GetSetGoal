7d70d098ff295be212068f3f66ca787cf9bf6cc0
Rana0001 <Magarprashant001@gmail.com>
2021-09-02 23:41:37 +0545

    User Panel

--- userpanel.py
+++ userpanel.py
@@ -17,11 +17,14 @@ def user_panel():
     bg_Img.place(x=0, y=0, relheight=1, relwidth=1)
 
 
-    frame = Frame(upanel,bd=3,highlightthickness= 5,highlightcolor="#1a2b63",highlightbackground="#1a2b63")
+    frame = Frame(upanel,bd=3,highlightthickness= 5,highlightcolor="#1a2b63",highlightbackground="#1a2b63",bg="#E9EDF5")
     frame.place(x=5,y=0,height= 835,width = 350)
-    label = Label(text=f"WELCOME",font= ("Futura",15,"bold"),fg = "#0066CA")
+    label = Label(text=f"WELCOME",font= ("Futura",15,"bold"),fg = "#0066CA",bg="#E9EDF5")
     label.place(x= 120, y= 200)
 
+    user_image = ImageTk.PhotoImage(Image.open("userpanel_graphics/user_icon.png"))
+    user_lbl = Label(upanel,image=user_image,bg="#E9EDF5")
+    user_lbl.place(x=110,y= 50)
 
     my_label = Label(upanel)
     my_label.place(x=390,y=35,height=370,width=650)
@@ -41,6 +44,31 @@ def user_panel():
     bbanner_img = ImageTk.PhotoImage(bottom_resized)
     bottom_lbl = Label(upanel,image= bbanner_img,bg= "#0066CA")
     bottom_lbl.place(x=460,y=740,height=75,width=950)
+
+# Dashboard Button
+    dash_img = Image.open("menu_icons/dashboard.png")
+    dash_resized = dash_img.resize((345, 68), Image.ANTIALIAS)
+    dash_image = ImageTk.PhotoImage(dash_resized)
+    btn_dash = Button(upanel,image = dash_image,bg="#E9EDF5",bd=0)
+    btn_dash.place(x= 12,y=250,width= 335,height=60)
+
+# League Button
+    league_img = Image.open("menu_icons/league.png")
+    league_resized = league_img.resize((340, 80), Image.ANTIALIAS)
+    league_image = ImageTk.PhotoImage(league_resized)
+    btn_league = Button(upanel, image=league_image, bg="#E9EDF5", bd=0)
+    btn_league.place(x=37, y=310, width=310, height=60)
+
+# Player of the month button
+    mvp_img = Image.open("menu_icons/player.png")
+    mvp_resized = mvp_img.resize((350, 75), Image.ANTIALIAS)
+    mvp_image = ImageTk.PhotoImage(mvp_resized)
+    btn_mvp = Button(upanel, image=mvp_image, bg="#E9EDF5", bd=0)
+    btn_mvp.place(x=37, y=370, width=305, height=60)
+
+    # bottom_lbl = Label(upanel,image= dash_image,bg= "#E9EDF5")
+    # bottom_lbl.place(x=30,y=250,height=40,width=320)
+
 
     upanel.mainloop()
 
